7b4f375272684a38ec46783bd067cefcde367d30
dodohalb <dominik.wissmann75@gmail.com>
2025-06-13 01:42:27 +0200

    chore(app): configure AppModule for JWT auth and Multer file uploads

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,10 +11,28 @@ import { AuthMySQL } from './repository/authMySQL';
 import { ChatMySQL } from './repository/chatMySQL';
 import { EventMySQL } from './repository/eventMySQL';
 import { UserMySQL } from './repository/userMySQL';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { randomUUID } from 'crypto';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    MulterModule.register({
+      limits: { fileSize: 10 * 1024 * 1024 }   // 10 MB
+    }),
+    PassportModule,
+    JwtModule.register({
+      secret: "secret-key",
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1d' },
+    })
+  ],
   controllers: [Gateway],
-  providers: [AuthService, EventService, UserService, ChatService, SwipeService, AuthMySQL, ChatMySQL, EventMySQL, UserMySQL],
+  providers: [
+    AuthService, EventService, UserService, ChatService, SwipeService, 
+    AuthMySQL, ChatMySQL, EventMySQL, UserMySQL,
+    JwtStrategy
+  ]
 })
 export class AppModule {}
