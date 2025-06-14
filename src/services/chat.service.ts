90fa9d427e38d3a4e49e343d925454c1eb46e8d1
dodohalb <dominik.wissmann75@gmail.com>
2025-06-14 02:13:28 +0200

    chore(chat-service): add method signature for sendMessage

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -1,6 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { promises } from 'dns';
+import { ChatMessage } from 'src/domainObjects/chatMessage';
 
 @Injectable()
 export class ChatService {
+    sendMessage(chatMessage: ChatMessage, eventId: number): Promise<ChatMessage> {
+      throw new Error('Method not implemented.');
+    }
     private logger = new Logger(ChatService.name);
 }
