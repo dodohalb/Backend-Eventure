a107aa73ba3f98f44126c281ab340bca77e73e60
dodohalb <dominik.wissmann75@gmail.com>
2025-06-14 02:12:29 +0200

    feat(chat-message): add new attributes

--- src/domainObjects/chatMessage.ts
+++ src/domainObjects/chatMessage.ts
@@ -1,5 +1,24 @@
-export class ChatMassage {
-    constructor(parameters) {
-        
-    }
+import { IsDate, IsOptional, IsString, ValidateNested } from "class-validator";
+import { User } from "./user";
+import { Type } from "class-transformer";
+
+
+export class ChatMessage {
+
+    @IsOptional()
+    messageId: number;
+
+    @ValidateNested()
+    @Type(() => User)
+    user: User;
+
+    @IsString()
+    content: string;
+
+    @Type(() => Date)
+    @IsDate()
+    timestamp: Date;
+
+    
+    constructor(){}
 }
