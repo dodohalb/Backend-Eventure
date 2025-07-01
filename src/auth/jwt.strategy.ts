cb28c8c057e2b7708c6f408cb16e134a6186f3c0
dodohalb <dominik.wissmann75@gmail.com>
2025-07-01 21:39:50 +0200

    fix bug in jwt strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,21 +1,23 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/domainObjects/user';
+import { DAO } from 'src/repository/dao';
 import { UserRepo } from 'src/repository/user.repo';
 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly userRepo: UserRepo) {
+  constructor(@Inject(UserRepo) private readonly userRepo: DAO<User>) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: 'secret-key',
     });
   }
 
-  async validate(payload: { sub: string }) {
-    const user = await this.userRepo.getUserByPhone(payload.sub);
+  async validate(payload: { sub: number }) {
+    const user = await this.userRepo.get(payload.sub);
     if (!user) throw new UnauthorizedException();
-    return user;
+    return user.id;
   }
 }
